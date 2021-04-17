0b3fb47197bcbd2bc92c3943d1944f091c928e09
Alexandre Martins <martinso.alex@gmail.com>
2021-04-17 03:00:28 -0300

    criptografia da senha com bcrypt

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 const express = require('express');
 const app = express();
-const bodyParser = require('body-parser');
 
 app.use(
-  bodyParser.urlencoded({
+  express.urlencoded({
     extended: true
   })
 );
